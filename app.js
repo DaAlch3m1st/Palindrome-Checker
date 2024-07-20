22767e8baad1a3ab0e7a563509d349cc22f58a60
alch3m1st <bryan.ale.3240@gmail.com>
2024-07-20 11:49:42 -0400

    Logic done - I create a function to check if the string is palindrome or not - an event when the user click the button check if the word is or not palindrome #Next: Style to the page

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
-// first I'm gonna do the project on the console (search how to reverse words)
+const checkBtn = document.getElementById('check-btn');
 
-function reverseString(str) {
-    return str.split('').reverse().join('');
+function palindromeChecker() {
+    // get the input value of the game
+    const inputText = document.getElementById('text-input').value
+    // Check if the value is palindrome doing with this code
+    const reverseInput = inputText.split('').reverse().join('').toLowerCase().replace(/[^a-z0-9]/g, '');
+
+    // get the element "p" to display the text if the number is palindrome or not
+    const result = document.getElementById('result');
+    // operator logic "if" the word is the same in reverse or not
+    if (inputText.trim() === '') {
+        result.innerHTML = 'Input is empty. Please enter something.'
+        console.log('Input is empty. Please enter something.');
+        // operator logic "elif if" to check if the word is palindrome 
+    } else if (reverseInput === inputText.toLowerCase().replace(/[^a-z0-9]/g, '')) {
+        result.innerHTML = `${inputText} is a palindrome`;
+        console.log(`${inputText} is palindrome`);
+    } else {
+        result.innerHTML = `${inputText} is not a palindrome`
+        console.log(`${inputText} isn't palindrome`);
+    }
 }
-const reversed = reverseString("ana");
-console.log(reversed);
+
+checkBtn.addEventListener('click', function() {
+    palindromeChecker();
+})
